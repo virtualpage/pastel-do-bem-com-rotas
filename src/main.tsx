6f18407c51c07998796c287f85c124c87c74b70d
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-12-05 10:17:35 -0300

    pagina home redirecioanndo para o instituto

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from './App.tsx'
+import HomeRedirect from './components/home-direciton.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/" element={<HomeRedirect />} />
         <Route path="/pasteldobem" element={<App />} />
       </Routes>
     </BrowserRouter>,
