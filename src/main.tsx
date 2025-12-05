a7fd9ea98d99f8225693fdd777c7872478ec5a1b
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-12-05 09:56:55 -0300

    primeiro commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/pastel-do-bem" element={<App />} />
+      </Routes>
+    </BrowserRouter>,
   </StrictMode>,
 )
